3bb15d9aad63d32f2ef7745d32656ae5c07079a7
hidwoc <heidischoi@gmail.com>
2021-07-14 15:12:25 -0400

    Define CRUD express routes

--- routes/post.js
+++ routes/post.js
@@ -4,5 +4,9 @@ import * as controllers from '../controllers/posts.js'
 const router = Router()
 
 router.get('/posts', controllers.getPosts)
+router.get('/posts/:id', controllers.getPosts)
+router.post('/posts', controllers.createPosts)
+router.put('/posts/:id', controllers.updatePosts)
+router.delete('/posts/:id', controllers.deletePosts)
 
 export default router
