70e32df1c1fd9a1dd40ba967039e5fd3ddb83db7
trishamarchena <galadrielvasquez@gmail.com>
2021-07-15 12:29:46 -0400

    set up axios call and api config

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,4 +1,16 @@
+import { useState, useEffect } from "react";
+import { getPosts } from "../../services/posts";
+
 const Home = () => {
+  const [ posts, setPosts ]= useState ([]);
+
+  useEffect(() =>{
+      const fetchPosts = async () =>{
+        const allPosts = await getPosts()
+        console.log(allPosts);
+      }
+  }, [] )
+
   return (
     <div>
       This is where Posts will go
