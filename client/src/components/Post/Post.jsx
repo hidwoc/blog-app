d0e14978662ad3b23e416343eb34b7222c95e91a
hidwoc <heidischoi@gmail.com>
2021-07-16 09:52:09 -0400

    create redirects file for deployment

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,15 +1,16 @@
 // import { Link } from "react-router-dom";
 import { deletePost } from "../../services/posts";
 
-const Post = ({_id, title, description}) => {
-
+const Post = ({ _id, title, description }) => {
   return (
     <div className="post-details">
       <div className="post-title">{title}</div>
       <div className="post-description">{description}</div>
       <div className="post-buttons">
         <button className="edit-button">EDIT</button>
-        <button className="delete-button" onClick={() => deletePost(_id)}>DELETE</button>
+        <button className="delete-button" onClick={() => deletePost(_id)}>
+          DELETE
+        </button>
       </div>
     </div>
   );
