543228d3c2f58db6122af6f6b1c1deb64cf6237e
Salman Ansari <salmanansari@Salmans-MBP.fios-router.home>
2021-07-16 11:14:04 -0400

    Done With EditPost component

--- client/src/screens/EditPost/EditPost.jsx
+++ client/src/screens/EditPost/EditPost.jsx
@@ -1,53 +1,57 @@
 import { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
-import { createPost, getPost, updatePost } from "../../services/posts";
+import { getPost, updatePost } from "../../services/posts";
 import { useHistory, useParams } from "react-router";
 
-const AddPost = () => {
+const EditPost = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const history = useHistory();
   const params = useParams();
-  useEffect(() => {
-    if (params.id) {
-      const editPost = getPost(params.id);
-      setTitle(editPost.title);
-      setDescription(editPost.description);
-    }
+  useEffect(async () => {
+    const editPost = await getPost(params.id);
+    setTitle(editPost.title);
+    setDescription(editPost.description);
   }, [params.id]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const newPost = {
+    const updatedPost = {
       title,
       description,
     };
-    await createPost(newPost);
-
-    // props.setToggleFetch((current) => !current);
+    await updatePost(params.id, updatedPost);
 
     history.push("/");
   };
   return (
     <Layout>
-        Edit Form
+      Edit Form
       <form onSubmit={handleSubmit}>
         <h1 id="formHead">Edit FORM</h1>
         <label id="formL" htmlFor="">
           Title:
         </label>
-        <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label id="formL" htmlFor="">
           Description:
         </label>
-        <input type="text" onChange={(e) => setDescription(e.target.value)} />
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <button type="submit" id="formBtn">
-          Add Post
+          Update Post
         </button>
       </form>
     </Layout>
   );
 };
 
-export default AddPost;
+export default EditPost;
